49464e4eeaa7788e0f403e404e9eeba1aba3b795
Raline Silva <ralinesilva1@gmail.com>
2021-08-20 16:27:59 -0300

    Exercicio3.bat

--- Exercicio3.bat
+++ Exercicio3.bat
@@ -1,2 +1,4 @@
 md "Todos os paises"
 cd "Todos os paises"
+
+md "Argentina" "Bolivia" "Brasil" "Chile" "Colombia" "Equador" "Guiana" "Guiana Francesa" "Paraguai" "Peru" "Suriname" "Uruguai" "Venezuela"
